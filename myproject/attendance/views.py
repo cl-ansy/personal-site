08305be2b5957e60e6fa73c24cdd836df8120864
Chris L <cslansin@gmail.com>
2013-04-24 19:34:46 +0000

    base template changes

--- myproject/attendance/views.py
+++ myproject/attendance/views.py
@@ -6,18 +6,18 @@ from attendance.models import CheckIn
 
 @csrf_exempt
 def attendance(request):
-    if request.method == 'POST':
-        #if CheckIn.objects.filter(studentID=request.POST['StudentID'],classID=request.POST['ClassID'],timestamp.date()=datetime.now().date()).distinct():
-         #   checkin = CheckIn.objects.get(studentID=request.POST['StudentID'],classID=request.POST['ClassID'],timestamp=datetime.now())
-        #else:
-        s_id = request.POST.get('StudentID', '')
-        c_id = request.POST.get('ClassID', '')
-        checkin = CheckIn()
-        checkin.studentID = 1324        
-        checkin.studentID = s_id
-        checkin.classID = c_id
-        checkin.timestamp = datetime.now()
-        checkin.save()
-    else:
-        checkin = CheckIn()
+#    if request.method == 'POST':
+#        #if CheckIn.objects.filter(studentID=request.POST['StudentID'],classID=request.POST['ClassID'],timestamp.date()=datetime.now().date()).distinct():
+#         #   checkin = CheckIn.objects.get(studentID=request.POST['StudentID'],classID=request.POST['ClassID'],timestamp=datetime.now())
+#        #else:
+#        s_id = request.POST.get('StudentID', '')
+#        c_id = request.POST.get('ClassID', '')
+#        checkin = CheckIn()
+#        checkin.studentID = 1324        
+#        checkin.studentID = s_id
+#        checkin.classID = c_id
+#        checkin.timestamp = datetime.now()
+#        checkin.save()
+#    else:
+    checkin = CheckIn()
     return render_to_response('attendance.html', {'checkin':checkin}, context_instance=RequestContext(request))
