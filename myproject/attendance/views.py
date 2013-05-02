c453c963e2980d76453d524d377c2963fbd85fd6
Chris L <cslansin@gmail.com>
2013-05-02 13:10:49 +0000

    added pagination

--- myproject/attendance/views.py
+++ myproject/attendance/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
@@ -16,5 +17,16 @@ def attendance(request):
             checkin.classID = request.POST.get('ClassID', '')
             checkin.timestamp = datetime.now()
             checkin.save()
-    checkins = CheckIn.objects.order_by("-timestamp")
+
+    checkin_list = CheckIn.objects.order_by("-timestamp")
+    paginator = Paginator(checkin_list, 10)
+
+    page = request.GET.get('page')
+    try:
+        checkins = paginator.page(page)
+    except PageNotAnInteger:
+        checkins = paginator.page(1)
+    except EmptyPage:
+        checkins = paginator.page(paginator.num_pages)
+
     return render_to_response('attendance.html', {'checkins':checkins}, context_instance=RequestContext(request))
